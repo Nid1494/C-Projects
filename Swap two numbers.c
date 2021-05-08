98bd6f2a3e001fbd2db9cf0d08f7a01864f6d163
Nid1494 <83866450+Nid1494@users.noreply.github.com>
2021-05-08 16:58:23 +0530

    Update Swap two numbers.c

--- Swap two numbers.c
+++ Swap two numbers.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
 main()
-
 {
 	int a,b;
 	printf("Enter two numbers: ");
 	scanf("%d",&a);
 	scanf("%d",&b);
 	printf("\n Before swapping a= %d and b= %d",a,b);
-	a+=b;
+	a+=b;  //Adds value of a and b and then stored in a
 	b=a-b;
-	a-=b;
+	a-=b;  //Value of b is subtracted from a and then stored in a
 	printf("\n After swapping a= %d and b= %d",a,b);
 	return 0;
 }
